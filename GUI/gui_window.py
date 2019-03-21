d7a6ee992129424b6ab930ca3cbc7b61ad1cee23
Richard Persaud <richardpersaud2010@hotmail.com>
2019-03-21 00:07:30 -0400

    changing Code

--- GUI/gui_window.py
+++ GUI/gui_window.py
@@ -1,32 +1,42 @@
-import tkinter as tk 
+import tkinter
+
 from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+ 
+window = Tk()
+window.geometry("1080x720")
+window.title("Welcome to LikeGeeks app")
 
 
-#top header menu 
-root = Tk()
-root.geometry("1080x720")
-menu = Menu(root) 
-root.config(menu=menu) 
-filemenu = Menu(menu) 
-menu.add_cascade(label='File', menu=filemenu) 
-filemenu.add_command(label='New') 
-filemenu.add_command(label='Open...') 
-filemenu.add_separator() 
-helpmenu = Menu(menu) 
-menu.add_cascade(label='Help', menu=helpmenu) 
-helpmenu.add_command(label='About')
-filemenu.add_command(label='Exit', command=root.quit)
-
-ourMessage ='This is our Message'
-messageVar = Message(root, text = ourMessage) 
-messageVar.config(bg='lightgreen') 
-messageVar.pack( )
+menu = Menu(window)
+new_item = Menu(menu)
+new_item.add_command(label='New')
+new_item.add_separator()
+new_item.add_command(label='Edit')
+menu.add_cascade(label='File', menu=new_item) 
+window.config(menu=menu)
 
 
+tab_control = ttk.Notebook(window)
+tab1 = ttk.Frame(tab_control)
+tab2 = ttk.Frame(tab_control)
+tab_control.add(tab1, text='First')
+tab_control.add(tab2, text='Second')
+lbl1 = Label(window, text="Hello page 1", font=("Arial Bold", 50))
+lbl1.grid(column=0, row=0)
+lbl2 = Label(window, text="Hello page 2", font=("Arial Bold", 50))
+lbl2.grid(column=0, row=0)
+tab_control.pack(expand=1, fill='both')
+
+
+  
+#essagebox.showinfo('Message title', 'Message content')
+ 
+#btn = Button(window,text='Click here', command=clicked)
+ 
+#btn.grid(column=0,row=0)
 
 
 
-
-
-root.mainloop() 
-
+window.mainloop()
