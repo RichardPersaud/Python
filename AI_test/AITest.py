a58ba3d365f3f8dafc80393c3b61c3a02aaf9939
RichardPersaud <persrich@sheridancollege.ca>
2019-06-17 23:01:39 -0400

    AI loops for conditions

--- AI_test/AITest.py
+++ AI_test/AITest.py
@@ -7,11 +7,11 @@ import requests
 
 import csv
 #clears console
-os.system('cls')
 
 
 
 def login():
+    os.system('cls')
 
     print('Welcome My Name Is Alpha Index, Please sign in\n')
 
@@ -55,10 +55,17 @@ def login():
             userin = str(input("Would you like guest access? (y or n) ")) 
             if (userin == 'y'):
                 print('\nThe username is temp, and the password is pass\n')
-            else: quit()
+                login()
+
+            elif (userin == 'n'):
+                login()
+
+            else: print('invalid input')
+            quit()
 
 # ---- Main ---- #
 login()
+
 
 # def main():
 
@@ -109,76 +116,84 @@ if __name__=='__main__':
 
 
 
+def AskUser():
+    #asks user for command
+    userInput = str(input("\nWhat would you like to do? \n1. go to ...\n2. search ...\n3. what is ...\n4. open ...\n5. weather ...\n  \n>>"))
 
-#asks user for command
-userInput = str(input("What would you like to do? \n1. go to ...\n2. search ...\n3. what is ...\n4. open ...\n5. weather ...\n  \n>>"))
+    if userInput == "go to":
+        print("where do you want to go on your PC?    example>( C:\example )")
+        mydir = str(input("go to..>> "))
 
-if userInput == "go to":
-    print("where do you want to go on your PC?    example>( C:\example )")
-    mydir = str(input("go to..>> "))
+        print("I cant find "+ (mydir)+ " on this PC as yet because my master has not tought me how to...")
+        AskUser()
 
-    print("I cant find "+ (mydir)+ " on this PC as yet because my master has not tought me how to...")
+    elif userInput == "search":
+        print("what do you want to search on the Web?   example( www.google.com )")
+        userInput = str(input("search..>> "))
 
-elif userInput == "search":
-    print("what do you want to search on the Web?   example( www.google.com )")
-    userInput = str(input("search..>> "))
+        #   searches the internet for the path your entering
 
-    #   searches the internet for the path your entering
-
-    url = userInput
-    chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-    webbrowser.get(chrome_path).open('https://'+(url))
-    print('Done!')
-
-
-elif userInput == "what is":
-    print("what is what?")
-    userInput = str(input("what is..>> "))
-
-    # finds the definition of something:
-
-    url = userInput
-    chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-    webbrowser.get(chrome_path).open('www.dictionary.com/browse/'+(url))
-    print('Done!')
-
-elif userInput == "open":
-    print("open what on desktop?")
-    userInput = str(input("open..>> "))
-
-    print("I cant find "+ (userInput)+ " on this PC as yet because my master has not tought me how to...")
+        url = userInput
+        chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+        webbrowser.get(chrome_path).open('https://'+(url))
+        print('Done!')
+        AskUser()
 
 
-# Asks for the city to find the current weather temp there 
+    elif userInput == "what is":
+        print("what is what?")
+        userInput = str(input("what is..>> "))
 
-elif userInput == "weather":
-    def weather_data(query):
+        # finds the definition of something:
 
-        res=requests.get('http://api.openweathermap.org/data/2.5/weather?'+query+'&APPID=b35975e18dc93725acb092f7272cc6b8&units=metric');
-        return res.json();
+        url = userInput
+        chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+        webbrowser.get(chrome_path).open('www.dictionary.com/browse/'+(url))
+        print('Done!')
+        AskUser()
 
-    def print_weather(result,city):
+    elif userInput == "open":
+        print("open what on desktop?")
+        userInput = str(input("open..>> "))
 
-        print("{}'s temperature: {}°C ".format(city,result['main']['temp']))
-        print("Wind speed: {} m/s".format(result['wind']['speed']))
-        print("Description: {}".format(result['weather'][0]['description']))
-        print("Weather: {}".format(result['weather'][0]['main']))
+        print("I cant find "+ (userInput)+ " on this PC as yet because my master has not tought me how to...")
+        AskUser()
 
-    def main():
 
-        city=input('Enter the city:')
-        print()
+    # Asks for the city to find the current weather temp there 
 
-        try:
-            query='q='+city;
-            w_data=weather_data(query);
-            print_weather(w_data, city)
+    elif userInput == "weather":
+        def weather_data(query):
+
+            res=requests.get('http://api.openweathermap.org/data/2.5/weather?'+query+'&APPID=b35975e18dc93725acb092f7272cc6b8&units=metric');
+            return res.json();
+
+        def print_weather(result,city):
+
+            print("{}'s temperature: {}°C ".format(city,result['main']['temp']))
+            print("Wind speed: {} m/s".format(result['wind']['speed']))
+            print("Description: {}".format(result['weather'][0]['description']))
+            print("Weather: {}".format(result['weather'][0]['main']))
+
+        def main():
+
+            city=input('\nEnter the city:')
             print()
 
-        except:
-            print('City name not found...')
+            try:
+                query='q='+city;
+                w_data=weather_data(query);
+                print_weather(w_data, city)
+                print()
 
-    if __name__=='__main__':
-        main()
+            except:
+                print('City name not found...')
+                main()
 
-else: print("Huh?")
+        if __name__=='__main__':
+            main()
+
+    else: print("\nHuh?\n")
+    AskUser()
+
+AskUser()
